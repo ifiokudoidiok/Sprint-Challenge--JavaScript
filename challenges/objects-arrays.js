733ac87e032a67db63481a2a6ff5d6f758b2b689
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-08-23 09:05:42 +0100

    Array task completed

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -74,7 +74,11 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
-console.log(universities)
+graduates.forEach(uni => {
+  universities.push(`${uni.university}`);
+});
+
+console.log(universities.sort())
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
 
@@ -83,11 +87,18 @@ Name email@example.com
 
 Log the result of your new array. */
 const contactInfo = [];
+graduates.forEach(info => {
+  contactInfo.push(`${info.first_name} ${info.email}`);
+});
 console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
 const uni = [];
+// console.log(uni);
+let students = graduates.filter((univ) => univ.university.indexOf('Uni') > -1);
+uni.push(students);
+console.log(`${students.length} Universities has the term Uni included in their names`);
 console.log(uni);
 
 
